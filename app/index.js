d0db8bb6d5b0188b5f5d21252eb82753ed068a57
Dani Ramirez <thebluebishop@gmail.com>
2017-10-19 21:59:59 +0200

    Restrict link creation. No loopbacks in an element nor in2in or out2out.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 var graph = new joint.dia.Graph;
 
 var paper = new joint.dia.Paper({
-
+    defaultRouter: { name: "manhattan" },
     el: $('#myholder'),
     width: 600,
     height: 600,
@@ -14,24 +14,39 @@ var paper = new joint.dia.Paper({
             { color: "#BBBBBB" }
         ]
 
-    }
+    },
+    validateConnection: validateConnection,
+    linkPinning: false
 });
+
+function validateConnection(cellViewS, magnetS, cellViewT, magnetT, end, linkView) {
+    // Prevent linking from output ports to input ports within one element.
+    if (cellViewS === cellViewT) return false;
+    // Prevent linking outputs
+    if (magnetS.getAttribute('port-group') === 'out' && magnetT.getAttribute('port-group') === 'out') return false;
+    return true;
+}
 
 var rect = new joint.shapes.devs.Model({
     position: { x: 100, y: 30 },
     size: { width: 100, height: 50 },
-    attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } },
+    attrs: { rect: { fill: 'blue' }, text: { text: 'Model A', fill: 'white' } },
     inPorts: ["A", "B"],
     outPorts: ["C", "D"]
-
 });
 
-var rect2 = rect.clone();
+var rect2 = new joint.shapes.devs.Model({
+    position: { x: 100, y: 30 },
+    size: { width: 100, height: 50 },
+    attrs: { rect: { fill: 'green' }, text: { text: 'Model B', fill: 'white' } },
+    inPorts: ["A", "B"],
+    outPorts: ["C", "D"]
+});
 
 var link1 = new joint.dia.Link({
     source: { id: rect.id, port: "A" },
-    target: { id: rect2.id, port: "D" },
-    router: { name: "manhattan" }
+    target: { id: rect2.id, port: "D" }
+
 });
 
 link1.label(0, {
